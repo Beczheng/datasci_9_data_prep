14fe48e6121d1f2179c8e332ad0eccf380ff53e2
beczheng <beckie.zheng101@gmail.com>
2023-11-22 23:12:37 +0000

    new changes

--- model_dev2/scripts/p2_transform.py
+++ model_dev2/scripts/p2_transform.py
@@ -8,22 +8,23 @@ print(df)
 # Getting the column names
 df.columns
 
-# Cleaning the column names
-df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace('-', '_').str.replace('/', '_').str.replace('(', '').str.replace(')', '').str.replace('.', '').str.replace(',', '')
+# Cleaning and renaming the column names
+df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace('-', '_').str.replace('/', '_').str.replace('(', '').str.replace(')', '').str.replace(',', '').str.replace('.', '')
+df = df.rename(columns={'neighborhood_uhf' : 'neighborhood', 'race_ethnicity' : 'race_and_ethnicity', 'hiv_diagnoses_per_100000_population' : 'hiv_diagnoses_per_100000', 'aids_diagnoses_per_100000_population' : 'aids_diagnoses_per_100000'})
 
 # Keeping columns
 to_keep = [
     'year',
     'borough',
-    'neighborhood_uhf',
+    'neighborhood',
     'sex',
-    'race_ethnicity',
+    'race_and_ethnicity',
     'total_number_of_hiv_diagnoses',
-    'hiv_diagnoses_per_100000_population',
+    'hiv_diagnoses_per_100000',
     'total_number_of_concurrent_hiv_aids_diagnoses',                      
     'proportion_of_concurrent_hiv_aids_diagnoses_among_all_hiv_diagnoses',
     'total_number_of_aids_diagnoses',                                       
-    'aids_diagnoses_per_100000_population'       
+    'aids_diagnoses_per_100000'       
 ]
 
 # Dropping missing values
@@ -35,7 +36,7 @@ df.dtypes
 
 # Changing data types
 df = df[to_keep]
-df[['total_number_of_hiv_diagnoses', 'hiv_diagnoses_per_100000_population', 'total_number_of_concurrent_hiv_aids_diagnoses', 'proportion_of_concurrent_hiv_aids_diagnoses_among_all_hiv_diagnoses', 'total_number_of_aids_diagnoses', 'aids_diagnoses_per_100000_population']] = df[['total_number_of_hiv_diagnoses', 'hiv_diagnoses_per_100000_population', 'total_number_of_concurrent_hiv_aids_diagnoses', 'proportion_of_concurrent_hiv_aids_diagnoses_among_all_hiv_diagnoses', 'total_number_of_aids_diagnoses', 'aids_diagnoses_per_100000_population']].apply(pd.to_numeric)
+df[['total_number_of_hiv_diagnoses', 'hiv_diagnoses_per_100000', 'total_number_of_concurrent_hiv_aids_diagnoses', 'proportion_of_concurrent_hiv_aids_diagnoses_among_all_hiv_diagnoses', 'total_number_of_aids_diagnoses', 'aids_diagnoses_per_100000']] = df[['total_number_of_hiv_diagnoses', 'hiv_diagnoses_per_100000', 'total_number_of_concurrent_hiv_aids_diagnoses', 'proportion_of_concurrent_hiv_aids_diagnoses_among_all_hiv_diagnoses', 'total_number_of_aids_diagnoses', 'aids_diagnoses_per_100000']].apply(pd.to_numeric)
 df[['year']] = df[['year']].astype('object')
 df.dtypes
 
@@ -65,18 +66,18 @@ df_mapping_date
 # Saving the mapping as a CSV for borough
 df_mapping_date.to_csv('model_dev2/data/processed/mapping_borough.csv', index=False)
 
-# Performing ordinal encoding on neighborhood_uhf
+# Performing ordinal encoding on neighborhood
 enc = OrdinalEncoder()
-enc.fit(df[['neighborhood_uhf']])
-df['neighborhood_uhf'] = enc.transform(df[['neighborhood_uhf']])
+enc.fit(df[['neighborhood']])
+df['neighborhood'] = enc.transform(df[['neighborhood']])
 
-# Creating a dataframe with mapping for neighborhood_uhf
-df_mapping_date = pd.DataFrame(enc.categories_[0], columns=['neighborhood_uhf'])
-df_mapping_date['neighborhood_uhf_ordinal'] = df_mapping_date.index
+# Creating a dataframe with mapping for neighborhood
+df_mapping_date = pd.DataFrame(enc.categories_[0], columns=['neighborhood'])
+df_mapping_date['neighborhood_ordinal'] = df_mapping_date.index
 df_mapping_date
 
-# Saving the mapping as a CSV for neighborhood_uhf
-df_mapping_date.to_csv('model_dev2/data/processed/mapping_neighborhood_uhf.csv', index=False)
+# Saving the mapping as a CSV for neighborhood
+df_mapping_date.to_csv('model_dev2/data/processed/mapping_neighborhood.csv', index=False)
 
 # Performing ordinal encoding on sex
 enc = OrdinalEncoder()
@@ -91,18 +92,18 @@ df_mapping_date
 # Saving the mapping as a CSV for sex
 df_mapping_date.to_csv('model_dev2/data/processed/mapping_sex.csv', index=False)
 
-# Performing ordinal encoding on race_ethnicity
+# Performing ordinal encoding on race_and_ethnicity
 enc = OrdinalEncoder()
-enc.fit(df[['race_ethnicity']])
-df['race_ethnicity'] = enc.transform(df[['race_ethnicity']])
+enc.fit(df[['race_and_ethnicity']])
+df['race_and_ethnicity'] = enc.transform(df[['race_and_ethnicity']])
 
-# Creating a dataframe with mapping for race_ethnicity
-df_mapping_date = pd.DataFrame(enc.categories_[0], columns=['race_ethnicity'])
-df_mapping_date['race_ethnicity_ordinal'] = df_mapping_date.index
+# Creating a dataframe with mapping for race_and_ethnicity
+df_mapping_date = pd.DataFrame(enc.categories_[0], columns=['race_and_ethnicity'])
+df_mapping_date['race_and_ethnicity_ordinal'] = df_mapping_date.index
 df_mapping_date
 
-# Saving the mapping as a CSV for race_ethnicity
-df_mapping_date.to_csv('model_dev2/data/processed/mapping_race_ethnicity.csv', index=False)
+# Saving the mapping as a CSV for race_and_ethnicity
+df_mapping_date.to_csv('model_dev2/data/processed/mapping_race_and_ethnicity.csv', index=False)
 
 # Saving the entire mapping as a CSV
 df.to_csv('model_dev2/data/processed/processed_HIV_AIDS_data.csv', index=False)
